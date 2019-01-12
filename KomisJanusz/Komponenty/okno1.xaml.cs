0cdae09d3693f6d98d9de39511fcb1594644d3df
Paweł Knapiński <cnapey@gmail.com>
2019-01-12 10:59:16 +0100

    nowe rzeczy

--- KomisJanusz/Komponenty/okno1.xaml.cs
+++ KomisJanusz/Komponenty/okno1.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using KomisJanuszDane;
+using KomisJanuszDane.Pojazdy.Samochody.Fordy;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -19,9 +21,70 @@ namespace KomisJanusz.Komponenty
     /// </summary>
     public partial class okno1 : Window
     {
+        private void WyslijKatalog(Pojazd pojazd)
+        {
+            marka.Text = pojazd.Marka;
+            model.Text = pojazd.Model;
+            cena.Text = pojazd.CenaSprzedazy.ToString();
+            rok.Text = pojazd.RokProdukcji.ToString();
+        }
+
+        private void WypelnijKatalog()
+        {
+            TreeViewItem pojazdy = new TreeViewItem();
+            pojazdy.Header = "Pojazdy w komisie";
+
+            TreeViewItem sam = new TreeViewItem();
+            sam.Header = "Samochody";
+
+            TreeViewItem mot = new TreeViewItem();
+            mot.Header = "Motocykle";
+
+            List<Pojazd> garaz = new List<Pojazd>();
+            garaz.Add(new Ranger(1972, 2000, 50));
+            garaz.Add(new Ranger(1972, 25000, 500));
+            garaz.Add(new Mustang(1972, 2000, 30));
+            garaz.Add(new Mustang(1922, 20, 50));
+
+            foreach (var samochod in garaz)
+            {
+                sam.Items.Add($"{samochod.Marka} {samochod.Model}");
+            }
+
+
+
+            pojazdy.Items.Add(sam);
+            pojazdy.Items.Add(mot);
+
+            Katalog.Items.Add(pojazdy);
+        }
         public okno1()
         {
             InitializeComponent();
+
+            Ranger test1 = new Ranger(1998, 3895, 21);
+            WyslijKatalog(test1);
+            WypelnijKatalog();
+        }
+
+        private void rok_text(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void model_text(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void marka_text(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void cena_text(object sender, TextChangedEventArgs e)
+        {
+
         }
     }
 }
